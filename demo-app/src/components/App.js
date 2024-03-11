a8c3ccb9760abafd6513651f1a62966c8b4cd4e4
Justin Phan <jkphan@uw.edu>
2024-03-10 18:34:24 -0700

    fix

--- demo-app/src/components/App.js
+++ demo-app/src/components/App.js
@@ -4,7 +4,7 @@ import About from './About';
 import NavBar from './NavBar';
 import Home from './Home';
 // import Resources from './Resources';
-import Quiz from './Quizcopy';
+import Quiz from './Quizcopy1';
 import Happy from './happy';
 import Sad from './sad';
 import Anger from './anger';
@@ -29,7 +29,7 @@ function App(props) {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/About" element={<About />} />
-          <Route path="/Quizcopy" element={<Quiz />} />
+          <Route path="/Quizcopy1" element={<Quiz />} />
           {/* <Route path="/Resources" element={<Resources />} /> */}
           <Route path="/happy" element={<Happy />} />
           <Route path="/sad" element={<Sad />} />
